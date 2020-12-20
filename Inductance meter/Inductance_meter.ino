788bb6cd40c7f02be751fddb4bb45d6b347d7acd
ME TECH <69456138+ME-TECH-ELECTRONICS@users.noreply.github.com>
2020-12-20 13:26:35 +0530

    2nd commit

--- Inductance meter/Inductance_meter.ino
+++ Inductance meter/Inductance_meter.ino
@@ -1,9 +1,9 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> 
 LiquidCrystal lcd(4,5,6,7,8,9);//RS=D4,E=D5,DB4=D6,DB4=D7,DB4=D8,DB4=D9
 
 //Global variables//
 double pulse, frequency, inductance;
-float cap_val = 220E-9; //Insert capacitor value here.(220E-9 = 220nF)
+float cap_val = 0.22E-6; //Insert capacitor value here.(220E-9 = 220nF)
 #define oscillate_in  3// Oscillation reading pin
 #define pulse_induct  2// Inductor pulsing pin
 
@@ -16,9 +16,10 @@ void setup() {
   lcd.setCursor(5,1);
   lcd.print("METER");
   delay(2000);
-
+  
 }
 void loop() {
+   lcd.clear();
    digitalWrite(pulse_induct, HIGH);
    delay(5);//give some time to charge inductor.
    digitalWrite(pulse_induct,LOW);
@@ -28,18 +29,36 @@ void loop() {
     frequency = 1E6/(2*pulse);
     inductance = 1/(cap_val*frequency*frequency*4.*3.14159*3.14159);
     inductance *= 1E6;
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Fr=");
-    lcd.setCursor(3,0);
-    lcd.print(frequency);
-    lcd.print("Hz");
-    lcd.setCursor(3,1);
-    lcd.print(inductance);
-    lcd.setCursor(0,1);
-    lcd.print("L=");
-    lcd.print("uH");
-    delay(250);
+    if(frequency >= 1000) {
+      lcd.setCursor(0,0);
+      lcd.print("Fr=");
+      lcd.setCursor(3,0);
+      lcd.print(frequency/1000);
+      lcd.print("KHz");
+    }
+    else {
+      lcd.setCursor(0,0);
+      lcd.print("Fr=");
+      lcd.setCursor(3,0);
+      lcd.print(frequency);
+      lcd.print("Hz");
+    }
+    if(inductance >= 1000) {
+      lcd.setCursor(0,1);
+      lcd.print("L=");
+      lcd.setCursor(2,1);
+      lcd.print(inductance/1000);
+      lcd.print("mH");  
+      delay(500);
+    }
+    else{
+      lcd.setCursor(0,1);
+      lcd.print("L=");
+      lcd.setCursor(2,1);
+      lcd.print(inductance);
+      lcd.print("uH");  
+      delay(500);
+    }      
    }
    else {
     lcd.clear();
@@ -50,7 +69,7 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("L=");
     lcd.setCursor(2,1);
-    lcd.print("0.00uH");
-    delay(250);
+    lcd.print("0.00uH");  
+    delay(500); 
    }
 }
--- Inductance meter/Inductance_meter_I2C.ino
+++ Inductance meter/Inductance_meter_I2C.ino
@@ -1,16 +1,16 @@
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> 
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x27,16,2);// sometimes the address will be 0x3F
 
 //Global variables//
 double pulse, frequency, inductance;
-float cap_val = 220E-9; //Insert capacitor value here.(220E-9 = 220nF)
+float cap_val = 0.22E-6; //Insert capacitor value here.(220E-9 = 220nF)
 #define oscillate_in  3// Oscillation reading pin
 #define pulse_induct  2// Inductor pulsing pin
 
 void setup() {
   pinMode(pulse_induct,OUTPUT);// Make D2 pin as output
-  pinMode(oscillate_in,INPUT);// Make D# pin as input
+  pinMode(oscillate_in,INPUT);// Make D3 pin as input
   lcd.init();                 // Initialize LCD display
   lcd.backlight();            // comment to turn off the backlight of LCD
   lcd.setCursor(3,0);
@@ -18,9 +18,10 @@ void setup() {
   lcd.setCursor(5,1);
   lcd.print("METER");
   delay(2000);
-
+  
 }
 void loop() {
+   lcd.clear();
    digitalWrite(pulse_induct, HIGH);
    delay(5);//give some time to charge inductor.
    digitalWrite(pulse_induct,LOW);
@@ -30,18 +31,36 @@ void loop() {
     frequency = 1E6/(2*pulse);
     inductance = 1/(cap_val*frequency*frequency*4.*3.14159*3.14159);
     inductance *= 1E6;
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Fr=");
-    lcd.setCursor(3,0);
-    lcd.print(frequency);
-    lcd.print("Hz");
-    lcd.setCursor(3,1);
-    lcd.print(inductance);
-    lcd.setCursor(0,1);
-    lcd.print("L=");
-    lcd.print("uH");
-    delay(250);
+    if(frequency >= 1000) {
+      lcd.setCursor(0,0);
+      lcd.print("Fr=");
+      lcd.setCursor(3,0);
+      lcd.print(frequency/1000);
+      lcd.print("KHz");
+    }
+    else {
+      lcd.setCursor(0,0);
+      lcd.print("Fr=");
+      lcd.setCursor(3,0);
+      lcd.print(frequency);
+      lcd.print("Hz");
+    }
+    if(inductance >= 1000) {
+      lcd.setCursor(0,1);
+      lcd.print("L=");
+      lcd.setCursor(2,1);
+      lcd.print(inductance/1000);
+      lcd.print("mH");  
+      delay(500);
+    }
+    else{
+      lcd.setCursor(0,1);
+      lcd.print("L=");
+      lcd.setCursor(2,1);
+      lcd.print(inductance);
+      lcd.print("uH");  
+      delay(500);
+    }      
    }
    else {
     lcd.clear();
@@ -52,7 +71,7 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("L=");
     lcd.setCursor(2,1);
-    lcd.print("0.00uH");
-    delay(250);
+    lcd.print("0.00uH");  
+    delay(500); 
    }
 }
